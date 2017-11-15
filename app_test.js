797d13c70d663e302aa80425817ea7bf5732858f
kkimu <kimura.kzm@gmail.com>
2017-11-15 19:42:50 +0900

    add triangulation

--- app_test.js
+++ app_test.js
@@ -8,3 +8,8 @@ test('1+2の足し算が出来る', async (t) => {
   const app = new App();
   t.is(await app.sum(1, 2), 3);
 });
+
+test('3+4の足し算が出来る', async (t) => {
+  const app = new App();
+  t.is(await app.sum(3, 4), 7);
+});
